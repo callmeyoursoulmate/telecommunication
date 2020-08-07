d2e4746a54fd069f1a5e907e00db5e678609323e
duyquangg <duyquangg20011998@gmail.com>
2020-08-07 15:20:17 +0700

    todolist

--- src/components/note/Header.js
+++ src/components/note/Header.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const Header = ({title, color}) => {
-  return (
-    <View style={styles.header}>
-      <Text style={[styles.text, {color}]}>{title}</Text>
-    </View>
-  );
+const Header = ({ title, color }) => {
+    return (
+        <View style={styles.header}>
+            <Text style={[styles.text, { color }]}>{title}</Text>
+        </View>
+    );
 };
 
-Header.defaultProps = {
-  title: 'Abc',
+Header.defaultProps = {
+    title: 'Abc',
 };
 
-const styles = StyleSheet.create({
-  header: {
-    height: 60,
-    padding: 15,
-    backgroundColor: 'darkslateblue',
-  },
-  text: {
-    fontSize: 23,
-    textAlign: 'center',
-  },
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        padding: 15,
+        backgroundColor: 'darkslateblue',
+    },
+    text: {
+        fontSize: 23,
+        textAlign: 'center',
+    },
 });
 
-export default Header;
+export default Header;
