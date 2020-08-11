f06e014762b3b5c0260cd0047f6c05dd42329498
duyquangg <duyquangg20011998@gmail.com>
2020-08-11 10:14:33 +0700

    u c

--- App.js
+++ App.js
@@ -6,24 +6,38 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from './src/AppRouter';
 
 import profileInitState from './src/reducers/profile/profileInitState';
+import themeInitState from './src/reducers/theme/themeInitState';
 import configureStore from './src/lib/configureStore';
 import { Provider } from 'react-redux';
 
-function getInitialState(){
+import localStorage from './src/lib/localStorage';
+
+function getInitialState() {
   const _initState = {
     profile: new profileInitState,
+    theme: new themeInitState
   };
   return _initState;
 }
-
 const store = configureStore(getInitialState());
 
 
 const App = () => {
+  const themeCurrent = store.getState().theme.theme;
+  const [theme, setTheme] = useState({themeCurrent});
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    let localTheme = await localStorage.getTheme(theme);
+    if(localTheme){
+      setTheme(localTheme);
+    }
+  }
   return (
     <Provider store={store}>
       <AppRouter />
